c6a8ee2e52cbcb2fc8ca30fe8f8552041cfc5748
kyeity <kyeity@gmail.com>
2022-07-11 11:08:02 +0300

    test

--- task2/task2.py
+++
@@ -1,6 +0,0 @@
-year = int(input("Введіть рік : "))
-
-if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-    print("Yes.")
-else:
-    print("No.")
--- task7/task7.py
+++
@@ -1,4 +0,0 @@
-num = int(input("Введіть цифру: "))
-for i in range(1, num + 1):
-  if num % i == 0:
-      print(i)
